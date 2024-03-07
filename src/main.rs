3ec05b218909e28aa1c8db072a0236abfa485e69
Gnana Ganesh <gnana.ganesh@thoughtworks.com>
2024-03-07 23:26:17 +0530

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     eprintln!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for  stream in listener.incoming() {
